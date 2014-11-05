59a9760d76307b933fea47db5dc53bf0c38d9d72
Francisco De La Cruz <helloquico@gmail.com>
2014-11-05 00:05:57 -0500

    Upload photo attachment to S3 with paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
-	has_attached_file :photo
+	has_attached_file :photo,
+	:storage => :s3,
+	:s3_credentials => "#{Rails.root}/config/s3.yml",
+	:path => ":attachment/:id/:style.:extension",
+	:bucket => "gemsy"
 	validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
